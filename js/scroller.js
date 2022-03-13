69a082e142e8656ed16ea92370fe58693bd5622c
Ishan5101 <74533998+Ishan5101@users.noreply.github.com>
2022-03-13 18:08:26 +0530

    Add files via upload

---
+++ js/scroller.js
@@ -0,0 +1,16 @@
+sts_bs("", [20080623, "images/", "", "blank.gif", 0, 1, 1, 5, "100%", "left", 1, 3, 123, 158, 0, 0, 0, 0, 0, 2000, 1, 24, 1, "stEffect(\"scroll(Rate=24,enabled=0,Duration=0.50)\")", -2, 60], ["none", 1, "#454545", "#FFFFFF", "", "repeat"]);
+sts_sca(["center", "middle", "center", "middle"], ["arrowl_out.gif", "arrowl_over.gif", "arrowl_gray.gif", 13, 13, "arrowr_out.gif", "arrowr_over.gif", "arrowr_gray.gif", 13, 13]);
+sts_ai("i0", [0, "", "", "_self", "topper-19-20-1.jpg", 123, 157, "center"], ["transparent", "9pt Verdana,Arial", "#000000", "none", "9pt Verdana,Arial", "#000000", "none"]);
+sts_ai("i10", [, , , , "topper-19-20-2.jpg"], [], "i0", "i0");
+sts_ai("i1", [, , , , "topper-19-20-3.jpg"], [], "i0", "i0");
+sts_ai("i2", [, , , , "topper-19-20-4.jpg"], [], "i0", "i0");
+sts_ai("i3", [, , , , "topper-19-20-5.jpg"], [], "i0", "i0");
+sts_ai("i4", [, , , , "topper-19-20-6.jpg"], [], "i0", "i0");
+sts_ai("i5", [, , , , "topper-19-20-7.jpg"], [], "i0", "i0");
+sts_ai("i6", [, , , , "topper-19-20-8.jpg"], [], "i0", "i0");
+sts_ai("i7", [, , , , "topper-19-20-9.jpg"], [], "i0", "i0");
+sts_ai("i8", [, , , , "topper-19-20-10.jpg"], [], "i0", "i0");
+sts_ai("i9", [, , , , "topper-19-20-11.jpg"], [], "i0", "i0");
+//sts_ai("i10",[,,,,"topper-pic11.jpg"],[],"i0","i0");
+
+sts_es();
